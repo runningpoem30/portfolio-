2d3345f0011531b7a9de21770102f02b496e991a
runningpoem30 <aryaanandpathak30@gmail.com>
2025-12-26 23:49:32 +0530

    gg

--- my-app/app/components/HackathonWins.tsx
+++ my-app/app/components/HackathonWins.tsx
@@ -1,11 +1,70 @@
 import React from 'react'
-
+import Image from "next/image";
+import { IconExternalLink } from "@tabler/icons-react"
 function HackathonWins() {
   return (
     <div>
-      <div  className='font-instrument font-bold lg:text-3xl lg:mt-10'>Hackathon Wins</div>
+      <div  className='font-instrument font-bold lg:text-3xl lg:mt-10 lg:mb-10'>Hackathon Wins</div>
+
+      {/* this is for devday hackathon */}
+      <div className='border-1 border-gray-300 lg:h-50 rounded-xl lg:p-2 lg:pt-4 lg:mb-10 '>
+        <div className='flex'>
+       <div className='border-1 border-gray-300 rounded-xl '>
+            <Image 
+            src="/images/hackthisfall.svg"
+            alt="Hack This Fall"
+            width={110}
+            height={20}
+            />
+        </div>
+        <div>
+            <h1 className='font-lilex lg:text-2xl lg:ml-3 font-bold'>Hackathon Winner</h1>
+             <h1 className='font-lilex lg:text-l lg:ml-3 font-bold'><a href='https://luma.com/rvz8bst9?tk=PbGwgj' target='black' rel='noopener noerferrer' className='text-blue-700'>DevDay Hackathon</a> by @apify and @hackthisfall</h1>
+        </div>
+        </div>
+        <p className='font-lilex lg:mt-5'>Built a tool that analyzes GitHub profiles—scraping repos, commits, issues, and languages to generate a yearly “GitHub Wrapped” summary. It highlights top languages, frameworks, activity patterns, and standout repositories, inspired by Spotify Wrapped. Fully automated with Apify, no GitHub API required. I won a keychron k10 lol .
+         </p>
+         <p className='font-lilex font-bold'>
+            Check it out : <a className='text-blue-700 font-bold text-m'  href='https://apify.com/stylish_panduri/gitwrap' target="_blank" rel="noopener noreferrer" >gitwrap</a>
+         </p>
+      </div>
+
+
+      {/* this is for hacktofuture hackathon */}
+            <div className='border-1 border-gray-300 lg:h-50 rounded-xl lg:p-2 lg:pt-4 lg:mb-10 '>
+        <div className='flex'>
+       <div className='border-1 border-gray-300 rounded-xl '>
+            <Image 
+            src="/images/hackthisfall.svg"
+            alt="Hack This Fall"
+            width={110}
+            height={20}
+            />
+        </div>
+        <div>
+            <h1 className='font-lilex lg:text-2xl lg:ml-3 font-bold'>1st Place</h1>
+             <h1 className='font-lilex lg:text-l lg:ml-3 font-bold'><a href='https://unstop.com/hackathons/hacktofuture-30-st-joseph-engineering-college-mangaluru-1380851' target='black' rel='noopener noerferrer' className='text-blue-700'>HackToFuture 3.0</a> by @St.Joseph's Engineering College, Mangalore</h1>
+        </div>
+        </div>
+        <p className='font-lilex lg:mt-5'>Built a decentralized platform that allows patients and doctors to securely upload, store, and instantly access medical records, enabling faster sharing and seamless retrieval when needed.
+         </p>
+         <p className='font-lilex font-bold'>
+            Check it out : <a className='text-blue-700 font-bold text-m'  href='https://apify.com/stylish_panduri/gitwrap' target="_blank" rel="noopener noreferrer" >gitwrap</a>
+         </p>
+      </div>
+
+
+
+
+
     </div>
   )
 }
 
 export default HackathonWins
+
+
+// https://apify.com/stylish_panduri/gitwrap
+
+
+// https://unstop.com/hackathons/hacktofuture-30-st-joseph-engineering-college-mangaluru-1380851
