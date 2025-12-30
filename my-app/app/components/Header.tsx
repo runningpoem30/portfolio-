ed0ea2d239574b191d12c83226446aa0fb13cd58
runningpoem30 <aryaanandpathak30@gmail.com>
2025-12-30 16:54:18 +0530

    change the margin

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -43,7 +43,7 @@ function ThemeToggleTwo() {
 
 function Header() {
   return ( 
-    <div id='header' className='pt-7 pb-10 lg:pb-1 rounded-10 ' >
+    <div id='header' className='pt-7 pb-10 lg:pb-1 rounded-10 ml-4' >
         <div className='pb-8 font-instrument '>
 <nav className="fixed  backdrop-blur-md  top-0 z-50 flex lg:gap-6 gap-2 lg:text-3xl justify-end dark:bg-black/50 bg-white/50 pb-7 pt-7 border-1 border-gray-100 dark:border-white/10 rounded-xl pl-1 pr-1 lg:pl-4 lg:pr-4">
             <a className='lg:mr-186 mr-28 cursor-pointer' href='#header'>aryapathak</a>
